8ce22f4768add933f4eacd70d54d09474cdd09da
pablo pevsner <pablo.pevsner@telecom-paristech.fr>
2020-11-22 21:44:00 +0100

    Ajout d'une map simple

--- michmich/src/components/Map.js
+++ michmich/src/components/Map.js
@@ -1,24 +1,68 @@
-import React, { Component } from 'react'
-
-import './Map.css';
-
-class Map extends Component {
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import './Map.css'
+const mapStyles = {
+    width: '100%',
+    height: '50%'
     
-    render() {
-        const { resultFromPython } = this.props
-        return (
-            <div className="pageMap">
-            {resultFromPython.map((result, index) => (
-              <li key={index}>
-                {result.name}: {result.rating}
-              </li>
-            ))} 
-          </div>
-        )
-    }
 };
 
-Map.propTypes = {
+export class MapContainer extends Component {
+
+    state = {
+        showingInfoWindow: false,  // Hides or shows the InfoWindow
+        activeMarker: {},          // Shows the active marker upon click
+        selectedPlace: {}          // Shows the InfoWindow to the selected place upon a marker
+    };
+
+    onMarkerClick = (props, marker, e) =>
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true,
+            text: marker.text || ''
+        });
+
+    onClose = props => {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            });
+        }
+    };
+    render() {
+
+        return (
+            <Map
+                google={this.props.google}
+                zoom={18}
+                style={mapStyles}
+                initialCenter={
+                    this.props.resultLocationInfo.geometry.location
+                    // { lat: 48.8587275, lng: 2.2944984 }
+                }
+            >
+
+                <Marker
+                    onClick={this.onMarkerClick}
+                    name={this.props.resultLocationInfo.name}
+                />
+                <InfoWindow
+                    marker={this.state.activeMarker}
+                    visible={this.state.showingInfoWindow}
+                    onClose={this.onClose}
+                >
+                    <div className={'placeCard'}>
+                        <h2>{this.state.selectedPlace.name}</h2>
+                        <h3>{this.props.resultLocationInfo.vicinity}</h3>
+                    </div>
+                </InfoWindow>
+            </Map>
+        );
+    }
 }
 
-export default Map;
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyCEUIo5QkTQbHL8eBolEF6C4i0fSsnarFk'
+})(MapContainer);
